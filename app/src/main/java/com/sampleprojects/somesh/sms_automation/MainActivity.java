29828cf777e2bf241d3c6c154d3a6bd4a7c25995
sms <somesh551@gmail.com>
2016-09-15 22:40:11 +0530

    initial commit

---
+++ app/src/main/java/com/sampleprojects/somesh/sms_automation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sampleprojects.somesh.sms_automation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
